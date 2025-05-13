f8e651b924e0a8fb2e588efeac147ac49c5e2d8a
anouther <155570107+anouther@users.noreply.github.com>
2025-05-13 18:01:19 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('Meet you again on our second 8th date.').split('');
+        const titles = ('Happy birthday, sayang!').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "Glad I can still have you today on the same date I asked you out. We went through ups and downs together and we got to know each other's characters and traits. There are no wishes or long words that I can write here other than hoping that you are always with me and always love me as much as I love you. Forgive me who may always make you confused with my attitude, I'm just afraid of losing you and want to really have you completely. Believe me there is no reason for me to reduce my love for you which is getting bigger and bigger every day, until maybe I will get used to feeling the pain of loving you too much.";
+            subtitle.innerText = "I’m so lucky to have you. Thank you for being mine. Here’s to more laughs, hugs, and sweet moments together. I love you 🤍.";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
